b8152c40f155ace1159acd645ea6debba27a8277
A.Karwowski <akk42@zips.uakron.edu>
2021-09-30 22:25:58 -0400

    create jsx src folder and app

---
+++ indecision-app/src/app.js
@@ -0,0 +1,8 @@
+console.log("app.js is running");
+
+// JSX - Javascript XML
+
+var template = <h1>This is JSX from app.js</h1>;
+const appRoot = document.getElementById("app");
+
+ReactDOM.render(template, appRoot);
