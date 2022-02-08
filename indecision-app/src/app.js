e2ba542c9b1c69766bae7f508c7ac6e5140b6570
A.Karwowski <akk42@zips.uakron.edu>
2022-02-08 16:55:17 -0700

    add 2 templates to main app.py

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,8 +1,48 @@
 console.log("app.js is running");
 
 // JSX - Javascript XML
+const app = {
+    title: "Some Title",
+    //subtitle: "Some Subtitle",
+    options: ['hell']
+}
+const template = (
+    <div>
+        <h1>{app.title}</h1>
+        {app.subtitle && <p> {app.subtitle}</p>}
+        <p>{app.options.length > 0 ? "Yes" : "No" }</p>
+        <ol>
+            <li>Item 1</li>
+            <li>Item 2</li>
+        </ol>
+    </div>
+);
+const user = {
+    name: 'Alex',
+    age: 24,
+    location: 'Utah'
+}
+const getLocation = (location) => {
+    if (location) {
+        return <p>Location: {location}</p>;
+    }
+}
+let count = 0;
 
-var template = <h1>This is JSX from app.js</h1>;
 const appRoot = document.getElementById("app");
 
-ReactDOM.render(template, appRoot);
+
+
+const renderCounterApp = () => {
+    const templateTwo = (
+    <div>
+        <h1>Count : {count}</h1>
+        <button id="my-id" onClick={() => {count++; renderCounterApp();}}>+1</button>
+        <button id="minus" onClick={() => {count--;renderCounterApp();}}>-1</button>
+        <button id="clear" onClick={() => {count = 0;renderCounterApp();}}>Reset</button>
+    </div>
+    ); 
+    ReactDOM.render(templateTwo, appRoot);
+}
+
+renderCounterApp()
