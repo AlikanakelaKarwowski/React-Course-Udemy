2589dad316e9a965b959c012e55bfadbc1391458
A.Karwowski <akk42@zips.uakron.edu>
2022-02-14 22:02:50 -0700

    class extending and function overridding

--- indecision-app/playground/es6-classes-1.js
+++ indecision-app/playground/es6-classes-1.js
@@ -12,9 +12,48 @@ class Person {
         return `${this.name} is ${this.age} years old`
     }
 }
+class Student extends Person {
+    constructor(name, age, major = 'Undecieded'){
+        super(name, age);
+        this.major = major;
+    }
+    hasMajor(){
+        return !!this.major
+    }
+    getDescription(){
+        let description = super.getDescription()
+        
+        if (this.hasMajor()) {
+            description += ` Their major is ${this.major}`
+        }
+        return description
+    }
+}
 
-const me = new Person('Alex Karwowski');
-const other = new Person();
+
+class Traveler extends Person {
+    constructor(name, age, home = 'Ohio') {
+        super(name, age)
+        this.home = home
+    }
+    hasHome(){
+        return !!this.home
+    }
+    getGreeting(){
+        let description = super.getDescription()
+
+        if (this.hasHome()){
+            description += ` I am visiting from ${this.home}`
+        }
+        return description
+    }
+}
+
+const me = new Student('Alex Karwowski', 25, 'CS');
+const other = new Traveler('Alex Karwowski', 22, 'Pootown');
+console.log(other.getGreeting())
 
 console.log(me)
 console.log(me.getGreeting())
+console.log(me.getDescription())
+console.log(me.hasMajor())
