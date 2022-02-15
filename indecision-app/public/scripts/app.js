e4121f2d4c552ca841eac893b5b51c7edfe00c26
A.Karwowski <akk42@zips.uakron.edu>
2022-02-14 20:39:22 -0700

    public script

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -4,28 +4,88 @@ console.log("app.js is running");
 
 // JSX - Javascript XML
 var app = {
-    title: "Some Title",
-    //subtitle: "Some Subtitle",
-    options: ["yes", "no"]
+    title: "Indecision App",
+    subtitle: "Put your life in the hands of a computer",
+    options: []
 };
-var template = React.createElement("div", null, React.createElement("h1",null,app.title),
-    app.subtitle && React.createElement("p",null," ",app.subtitle),
-    React.createElement("p",null,app.options.length > 0 ? "Yes" : "No"),
-    React.createElement("ol",null,
-        React.createElement("li",null,"Item 1"),
-        React.createElement("li",null,"Item 2"))
-);
-var count = 0;
 
-var appRoot = document.getElementById("app");
+var onFormSubmit = function onFormSubmit(e) {
+    e.preventDefault();
 
-var renderCounterApp = function renderCounterApp() {
-    var templateTwo = React.createElement("div",null,React.createElement("h1",null,"Count : ",count),
-    React.createElement("button",{ id: "my-id", onClick: function onClick() {count++;renderCounterApp();} },"+1"),
-    React.createElement("button",{ id: "minus", onClick: function onClick() {count--;renderCounterApp();} },"-1"),
-    React.createElement("button",{ id: "clear", onClick: function onClick() {count = 0;renderCounterApp();} },"Reset")
+    var option = e.target.elements.option.value;
+    if (option) {
+        app.options.push(option);
+        console.log(option);
+        e.target.elements.option.value = "";
+        render();
+    }
+};
+
+var removeAll = function removeAll() {
+    app.options = [];
+    render();
+};
+
+var makeDecision = function makeDecision() {
+    var randomNum = Math.floor(Math.random() * app.options.length);
+    var option = app.options[randomNum];
+    console.log(randomNum);
+    alert(option);
+};
+
+var render = function render() {
+    var template = React.createElement(
+        "div",
+        null,
+        React.createElement(
+            "h1",
+            null,
+            app.title
+        ),
+        app.subtitle && React.createElement(
+            "p",
+            null,
+            app.subtitle
+        ),
+        React.createElement(
+            "p",
+            null,
+            app.options.length > 0 ? "Here are your options" : "No options"
+        ),
+        React.createElement(
+            "button",
+            { disabled: app.options.length === 0, onClick: makeDecision },
+            "What Should I Do"
+        ),
+        React.createElement(
+            "button",
+            { onClick: removeAll },
+            "Remove All"
+        ),
+        React.createElement(
+            "ol",
+            null,
+            app.options.map(function (option) {
+                return React.createElement(
+                    "li",
+                    { key: option },
+                    option
+                );
+            })
+        ),
+        React.createElement(
+            "form",
+            { onSubmit: onFormSubmit },
+            React.createElement("input", { type: "text", name: "option" }),
+            React.createElement(
+                "button",
+                null,
+                "Add Option"
+            )
+        )
     );
-    ReactDOM.render(templateTwo, appRoot);
+    var appRoot = document.getElementById("app");
+    ReactDOM.render(template, appRoot);
 };
 
-renderCounterApp();
+render();
