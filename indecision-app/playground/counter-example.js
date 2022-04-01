6fac0932210df903cf26a14f70dac4e195d32ae2
A.Karwowski <akk42@zips.uakron.edu>
2022-03-31 18:45:29 -0600

    use localstate and componentdidmount/update

--- indecision-app/playground/counter-example.js
+++ indecision-app/playground/counter-example.js
@@ -5,8 +5,22 @@ class Counter extends React.Component {
         this.handleSubOne = this.handleSubOne.bind(this)
         this.handleReset = this.handleReset.bind(this)
         this.state = {
-            count: props.count
+            count: 0
         }
+    }
+    componentDidMount() {
+        const stringCount = localStorage.getItem('count')
+        const count = parseInt(stringCount, 10)
+        
+        if (!isNaN(count)) {
+            this.setState(() => {return {count}})
+        }
+    }
+    componentDidUpdate(prevProps, prevState) {
+        if (prevState.count !== this.state.count) {
+            localStorage.setItem('count', this.state.count)
+        }
+
     }
     render() {
         return (
@@ -41,9 +55,6 @@ class Counter extends React.Component {
     }
 }
 
-Counter.defaultProps = {
-    count: 10
-}
 
 
 ReactDOM.render(<Counter/>, document.getElementById('app'))
