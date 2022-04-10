4605a4579aab2695002cd6863799af901748968e
A.Karwowski <akk42@zips.uakron.edu>
2022-04-10 17:47:37 -0600

    typo

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const template = <p>This is a JSX from webpacka</p> 
+const template = <p>This is a JSX from webpack</p> 
 ReactDOM.render(template, document.getElementById('app'));
