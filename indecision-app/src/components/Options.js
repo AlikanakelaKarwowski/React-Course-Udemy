b2c97871a5dde72ff3c2b1a16e13b7f8435c3a68
A.Karwowski <akk42@zips.uakron.edu>
2022-05-01 16:06:33 -0600

    add button and button--link styles

--- indecision-app/src/components/Options.js
+++ indecision-app/src/components/Options.js
@@ -2,10 +2,19 @@ import React from 'react';
 import Option from './Option'
 const Options = (props) => (
         <div>
-            <button onClick={props.handleDeleteOptions}>Remove All</button>
+            <button 
+                className="button button--link"
+                onClick={props.handleDeleteOptions}
+            >
+                Remove All
+            </button>
             {props.options.length === 0 && <p>Please Add an Option to Get Started</p>}
             {props.options.map((option) => (
-                <Option key={option} optionText={option} handleDeleteOption={props.handleDeleteOption} />
+                <Option 
+                    key={option} 
+                    optionText={option}
+                    handleDeleteOption={props.handleDeleteOption}
+                />
             ))}
         </div>
     );
