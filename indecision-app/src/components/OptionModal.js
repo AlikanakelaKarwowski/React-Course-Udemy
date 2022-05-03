fd2ebcbd95b671f676dd2f8a32d3189a8a809e92
A.Karwowski <akk42@zips.uakron.edu>
2022-05-02 18:30:28 -0600

    add class names to style content in modal

--- indecision-app/src/components/OptionModal.js
+++ indecision-app/src/components/OptionModal.js
@@ -9,6 +9,9 @@ const OptionModal = (props) => (
         closeTimeoutMS={200}
         className="modal"
         >
+            <h3 className="modal__title">Selected Option</h3>
+            {props.selectedOption && <p className="modal__body">{props.selectedOption}</p>}
+            <button className="button" onClick={props.handleClearSelectedOption}>OKAY</button>
     </Modal>
 )
 
