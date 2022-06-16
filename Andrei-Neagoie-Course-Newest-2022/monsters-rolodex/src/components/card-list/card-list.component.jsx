949bf284fdb98b3c895f44db4ae1508d7e7b74a7
A.Karwowski <akk42@zips.uakron.edu>
2022-06-15 20:36:16 -0600

    add in images

--- Andrei-Neagoie-Course-Newest-2022/monsters-rolodex/src/components/card-list/card-list.component.jsx
+++ Andrei-Neagoie-Course-Newest-2022/monsters-rolodex/src/components/card-list/card-list.component.jsx
@@ -3,13 +3,21 @@ import { Component } from "react";
 class CardList extends Component {
     render() {
         const { monsters } = this.props;
-        console.log('render from cardlist')
+        console.log("render from cardlist");
         return (
-        <div>
-            {monsters.map( monster => (
-                <h1 key={monster.id}>{monster.name}</h1>
-            ))}
-        </div>)
+            <div className="card-list">
+                {monsters.map((monster) => (
+                    <div className="card-container" key={monster.id}>
+                        <img
+                            alt={`monster ${monster.name}`}
+                            src={`https://robohash.org/${monster.id}?set=set4&size=180x180`}
+                        />
+                        <h1>{monster.name}</h1>
+                        <p>{monster.email}</p>
+                    </div>
+                ))}
+            </div>
+        );
     }
 }
 
