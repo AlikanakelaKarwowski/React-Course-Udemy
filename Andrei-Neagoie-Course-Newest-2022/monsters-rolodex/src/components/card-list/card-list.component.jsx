02a00631621d70deaf7cb618a3acd5d4d7b6aceb
A.Karwowski <akk42@zips.uakron.edu>
2022-06-18 13:32:59 -0600

    change to functional components

--- Andrei-Neagoie-Course-Newest-2022/monsters-rolodex/src/components/card-list/card-list.component.jsx
+++ Andrei-Neagoie-Course-Newest-2022/monsters-rolodex/src/components/card-list/card-list.component.jsx
@@ -1,9 +1,9 @@
 import Card from "../card/card.component";
 import "./card-list.styles.css";
-const CardList = ({monsters}) => (
+const CardList = ({katz}) => (
         <div className="card-list">
-            {monsters.map((monster) => {
-                return <Card monster={monster} />;
+            {katz.map((kat) => {
+                return <Card kat={kat} />;
             })}
         </div>
 );
